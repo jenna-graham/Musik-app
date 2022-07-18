1a2dc67f8005673734df99cd807b72826922b7d5
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-07-18 15:30:30 -0700

    testing

--- functions/spotify.js
+++ functions/spotify.js
@@ -1,5 +1,12 @@
 const fetch = require('node-fetch');
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv');
+dotenv.config();
+
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': 'Content-Type',
+  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+};
 
 exports.handler = async (event, context) => {
   
@@ -7,18 +14,20 @@ exports.handler = async (event, context) => {
   const refreshToken = process.env.SPOTIFY_REFRESH_TOKEN;
   const auth = Buffer.from(`${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_CLIENT_SECRET}`).toString('base64');
 
+
   const tokenEndPoint = `https://accounts.spotify.com/api/token`;
 
   const options = {
     method: 'POST',
     headers: {
       Authorization: `Basic ${auth}`,
-      'Content-Type': 'application/x-www.-form-urlencoded'
+      'Content-Type': 'application/x-www-form-urlencoded'
     },
     body: `grant_type=refresh_token&refresh_token=${refreshToken}&redirect_uri=${encodeURI(process.env.URL,
       +'/.netlify/functions/callback')}`,
   };
   const accessToken = await fetch(tokenEndPoint, options)
+  
     .then((res) => res.json())
     .then((json) => {
       return json.access_token;
@@ -27,21 +36,25 @@ exports.handler = async (event, context) => {
       console.error(error);
     });
 
-  const pleaseWork = `https://api.spotify.com/v1/search?type=artist&include_external=audio&q=beyonce`;
+  console.log(accessToken);
+
+
+  const pleaseWork = `https://api.spotify.com/v1/search?type=artist&q=beyonce`;
 
   return fetch(`${pleaseWork}`, {
-    method: 'Get',
+    method: 'GET',
     headers: {
       Authorization: `Bearer ${accessToken}`,
     },
   })
     .then((res) => res.json())
     .then(json => {
-      console.log(json);
+      console.log('testing json', json);
     })
     .then(json => {
       return {
         statusCode: 200,
+        headers,
         body: JSON.stringify(json)
       };
     });
