f6f335fc1525578813789beb308ada545db5dc21
Mariah <mariahschock@gmail.com>
2022-07-19 12:33:08 -0700

    some styling

--- src/ArtistList.js
+++ src/ArtistList.js
@@ -4,15 +4,19 @@ import { Link } from 'react-router-dom';
 export default function ArtistList({ artists }) {
   console.log(artists);
   return (
-    <div>
+    <div className="artist-list">
       {
         artists.map((artist, i) => (
           <div key={artist.id + i} className="artist">
             <Link to={`ArtistDetails/${artist.id}`}>
               <h3>{artist.name}</h3>
-             
+              {/* <img src={artist.images[0].url} /> */}
               <div>{artist.images.map((image, i) => 
-                <div key={image.height + i}>{image.height === 320 && <img src={image.url}/>}</div>)}
+                <div key={image.height + i}>
+                    
+                  {console.log(image)}
+
+                  {image.height > 600 && image.height < 700 && <img src={image.url}/>}</div>)}
               </div>
             </Link>
           </div>
