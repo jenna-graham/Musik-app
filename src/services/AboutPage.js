bbd65fc1c458a68fa69624ffb8034b80245d0ff9
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-07-20 12:42:43 -0700

    added minor styling

--- src/services/AboutPage.js
+++ src/services/AboutPage.js
@@ -9,9 +9,9 @@ export default function AboutPage() {
         <img src="/Aaron1.jpg" />
         <p className='dev-p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <span>
-          <a href="https://www.linkedin.com/in/aaron-enyetu/
+          <a className="dev-link" href="https://www.linkedin.com/in/aaron-enyetu/
 " target='blank'>LinkedIn<AiFillLinkedin /></a>
-          <a href="https://github.com/aaronEnyetu/
+          <a className="dev-link" href="https://github.com/aaronEnyetu/
 " target='blank'>GitHub<DiGithubBadge /></a>
         </span>
       </div>
@@ -20,8 +20,10 @@ export default function AboutPage() {
         <img src="/Jenna.jpeg" />
         <p className='dev-p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <span>
-          <a href="/" target='blank'> LinkedIn<AiFillLinkedin /></a>
-          <a href="/" target='blank'>GitHub<DiGithubBadge /></a>
+          <a href="https://www.linkedin.com/in/jenna-lee-graham/
+" target='blank'> LinkedIn<AiFillLinkedin /></a>
+          <a href="https://github.com/jenna-graham
+" target='blank'>GitHub<DiGithubBadge /></a>
         </span>
       </div>
       <div className='single-dev'>
@@ -29,13 +31,13 @@ export default function AboutPage() {
         <img src="/Mariah.jpg" />
         <p className='dev-p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <span>
-          <a href="/" target='blank'>LinkedIn<AiFillLinkedin /></a>
-          <a href="/" target='blank'>GitHub<DiGithubBadge /></a>
+          <a href="https://www.linkedin.com/in/mariah-schock" target='blank'>LinkedIn<AiFillLinkedin /></a>
+          <a href="https://github.com/mariahschock" target='blank'>GitHub<DiGithubBadge /></a>
         </span>
       </div>
       <div className='single-dev'>
         <h2>Sebastian Simek</h2>
-        <img src="/" />
+        <img src="/crab.jpg" />
         <p className='dev-p'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         <span>
           <a href="https://www.linkedin.com/in/sebastian-simek/" target='blank'>LinkedIn<AiFillLinkedin /></a>
