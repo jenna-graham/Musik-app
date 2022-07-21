cbcaeabbdaecfd5f78023c9ea9fa7ee2fedbc5b5
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-07-21 15:02:29 -0700

    added some lost things back

--- src/FavoritesPage.js
+++ src/FavoritesPage.js
@@ -1,6 +1,7 @@
 import { useDataContext } from './ContextProvider';
 import { Link } from 'react-router-dom';
 import DeleteButtons from './DeleteButton';
+import UserProfile from './UserProfile';
 
 export default function FavoritesPage() {
   const { favorites, handleDeleteFavorite } = useDataContext();
@@ -9,13 +10,14 @@ export default function FavoritesPage() {
     <div className="fave-list">
       {favorites.map((favorite, i) => (
         <div className="fave-artist" key={favorite.name + i}>
-          <Link>
+          <Link to={`/artist/${favorite.artist_id}`}>
             <h3>{favorite.name}</h3>
             <img src={favorite.images} />
           </Link>
           <DeleteButtons onClick={() => handleDeleteFavorite(favorite.id)}>Remove from Favorites</DeleteButtons>
         </div>
       ))}
+      <UserProfile />
     </div>
   );
 }
