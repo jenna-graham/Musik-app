2fa16b982505219d7ca7226126439f465d9e51fe
Mariah <mariahschock@gmail.com>
2022-07-20 22:09:32 -0700

    artist details/album styling

--- src/ArtistDetails.js
+++ src/ArtistDetails.js
@@ -15,18 +15,22 @@ export default function ArtistDetails() {
 }, [id]); //eslint-disable-line
 
   return (
-    <div>
-      <h1>{singleArtist.name}</h1>
-      <div>{singleArtist.images && singleArtist.images[0] && <img src={singleArtist.images[0].url} />}</div>
-      <h3>Genres:</h3>
-      <ul>
-        <li>{singleArtist.genres && singleArtist.genres[0]}</li>
-        <li>{singleArtist.genres && singleArtist.genres[1]}</li>
-        <li>{singleArtist.genres && singleArtist.genres[2]}</li>
-       
-      </ul>
-      <AlbumsList singleArtist={singleArtist}/>
-
+    <div className="artist-details">
+      <div className="single-artist">
+        <div>{singleArtist.images && singleArtist.images[0] && <img src={singleArtist.images[0].url} />}</div>
+        <div className="single-artist-info">
+          <h1>{singleArtist.name}</h1>
+          <h3>Genres:</h3>
+          <ul>
+            <li>{singleArtist.genres && singleArtist.genres[0]}</li>
+            <li>{singleArtist.genres && singleArtist.genres[1]}</li>
+            <li>{singleArtist.genres && singleArtist.genres[2]}</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <AlbumsList singleArtist={singleArtist}/>
+      </div>
     </div>
   );
 }
