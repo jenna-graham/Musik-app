be07f725d13e9cda12f3b5462633e7879fffddbd
Mariah <mariahschock@gmail.com>
2022-07-21 21:15:12 -0700

    styled the about us page

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -3,91 +3,97 @@ import { AiFillLinkedin } from 'react-icons/ai';
 
 export default function AboutPage() {
   return (
-    <div className="about-developers">
-      <div className="single-dev">
-        <h2>Aaron Enyetu</h2>
-        <img className="dev-image" src="/Aaron1.jpg" />
-        <p className="dev-p"></p>
-        <span>
-          <a
-            className="dev-link"
-            href="https://www.linkedin.com/in/aaron-enyetu/
+    <div>
+      <header className="jams">
+        <h1>Meet the J A M S !</h1>
+      </header>
+      <div className="about-developers">
+       
+        <div className="single-dev">
+          <h2>Jenna Graham</h2>
+          <img className="dev-image" src="/Jenna.jpeg" />
+          <p className="dev-p">Jenna is a Full Stack Software Developer based in Portland, Oregon. When she is not in front of the computer screen she enjoys yoga, photography and teaches a dance called Kizomba.</p>
+          <span>
+            <a
+              className="dev-link"
+              href="https://www.linkedin.com/in/jenna-lee-graham/
 "
-            target="blank"
-          >
+              target="blank"
+            >
+              {' '}
             LinkedIn
-            <AiFillLinkedin />
-          </a>
-          <a
-            className="dev-link"
-            href="https://github.com/aaronEnyetu/
+              <AiFillLinkedin />
+            </a>
+            <a
+              className="dev-link"
+              href="https://github.com/jenna-graham
 "
-            target="blank"
-          >
+              target="blank"
+            >
             GitHub
-            <DiGithubBadge />
-          </a>
-        </span>
-      </div>
-      <div className="single-dev">
-        <h2>Jenna Graham</h2>
-        <img className="dev-image" src="/Jenna.jpeg" />
-        <p className="dev-p"></p>
-        <span>
-          <a
-            className="dev-link"
-            href="https://www.linkedin.com/in/jenna-lee-graham/
+              <DiGithubBadge />
+            </a>
+          </span>
+        </div>
+        <div className="single-dev">
+          <h2>Aaron Enyetu</h2>
+          <img className="dev-image" src="/Aaron1.jpg" />
+          <p className="dev-p">Aaron is a Software Engineer based in Austin, Texas. He is passionate about sustainable organic agriculture.</p> <br /> <br />
+          <span>
+            <a
+              className="dev-link"
+              href="https://www.linkedin.com/in/aaron-enyetu/
 "
-            target="blank"
-          >
-            {' '}
+              target="blank"
+            >
             LinkedIn
-            <AiFillLinkedin />
-          </a>
-          <a
-            className="dev-link"
-            href="https://github.com/jenna-graham
+              <AiFillLinkedin />
+            </a>
+            <a
+              className="dev-link"
+              href="https://github.com/aaronEnyetu/
 "
-            target="blank"
-          >
+              target="blank"
+            >
             GitHub
-            <DiGithubBadge />
-          </a>
-        </span>
-      </div>
-      <div className="single-dev">
-        <h2>Mariah Schock</h2>
-        <img className="dev-image" src="/Mariah.jpg" />
-        <p className="dev-p"></p>
-        <span>
-          <a className="dev-link" href="https://www.linkedin.com/in/mariah-schock" target="blank">
+              <DiGithubBadge />
+            </a>
+          </span>
+        </div>
+        <div className="single-dev">
+          <h2>Mariah Schock</h2>
+          <img className="dev-image" src="/Mariah.jpg" />
+          <p className="dev-p">Mariah is a Full Stack Software Engineer based in Sherwood, Oregon. Outside of coding, she enjoys hiking and paddle-boarding.</p> <br /> <br />
+          <span>
+            <a className="dev-link" href="https://www.linkedin.com/in/mariah-schock" target="blank">
             LinkedIn
-            <AiFillLinkedin />
-          </a>
-          <a className="dev-link" href="https://github.com/mariahschock" target="blank">
+              <AiFillLinkedin />
+            </a>
+            <a className="dev-link" href="https://github.com/mariahschock" target="blank">
             GitHub
-            <DiGithubBadge />
-          </a>
-        </span>
-      </div>
-      <div className="single-dev">
-        <h2>Sebastian Simek</h2>
-        <img className="dev-image" src="/crab.jpg" />
-        <p className="dev-p"></p>
-        <span>
-          <a
-            className="dev-link"
-            href="https://www.linkedin.com/in/sebastian-simek/"
-            target="blank"
-          >
+              <DiGithubBadge />
+            </a>
+          </span>
+        </div>
+        <div className="single-dev">
+          <h2>Sebastian Simek</h2>
+          <img className="dev-image" src="/crab.jpg" />
+          <p className="dev-p">Sebastian is a Full Stack Software engineer based in Portland Oregon. Outside of coding he enjoys kayaking and playing tennis.</p> <br /> <br />
+          <span>
+            <a
+              className="dev-link"
+              href="https://www.linkedin.com/in/sebastian-simek/"
+              target="blank"
+            >
             LinkedIn
-            <AiFillLinkedin />
-          </a>
-          <a className="dev-link" href="https://github.com/sebastian-Simek/" target="blank">
+              <AiFillLinkedin />
+            </a>
+            <a className="dev-link" href="https://github.com/sebastian-Simek/" target="blank">
             GitHub
-            <DiGithubBadge />
-          </a>
-        </span>
+              <DiGithubBadge />
+            </a>
+          </span>
+        </div>
       </div>
     </div>
   );
